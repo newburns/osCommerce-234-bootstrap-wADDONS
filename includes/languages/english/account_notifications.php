ef0f7c0fbcee6bb0b16ad711bc3ad8d098027b8f
Thomas Mccaffery <travelagloo@verizon.net>
2015-04-03 20:18:32 -0400

    Cleaned up language files

--- includes/languages/english/account_notifications.php
+++ includes/languages/english/account_notifications.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2003 osCommerce
+  Copyright (c) 2015 osCommerce
 
   Released under the GNU General Public License
 */
@@ -19,7 +19,7 @@ define('MY_NOTIFICATIONS_TITLE', 'My Product Notifications');
 define('MY_NOTIFICATIONS_DESCRIPTION', 'The product notification list allows you to stay up to date on products you find of interest.<br /><br />To be up to date on all product changes, select <strong>Global Product Notifications</strong>.');
 
 define('GLOBAL_NOTIFICATIONS_TITLE', 'Global Product Notifications');
-define('GLOBAL_NOTIFICATIONS_DESCRIPTION', 'Receive notifications on all available products.');
+define('GLOBAL_NOTIFICATIONS_DESCRIPTION', 'Recieve notifications on all available products.');
 
 define('NOTIFICATIONS_TITLE', 'Product Notifications');
 define('NOTIFICATIONS_DESCRIPTION', 'To remove a product notification, clear the products checkbox and click on Continue.');
--- includes/languages/english/login.php
+++ includes/languages/english/login.php
@@ -5,10 +5,11 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2014 osCommerce
+  Copyright (c) 2015 osCommerce
 
   Released under the GNU General Public License
 */
 
 define('NAVBAR_TITLE', 'Login');
 define('HEADING_TITLE', 'Welcome, Please Sign In');
+?>
